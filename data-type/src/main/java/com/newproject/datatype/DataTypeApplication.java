fb80e82e5900726041120bd1eb6584ea1a64b61d
leonardoporcari <leonardomaria.porcari@mail.polimi.it>
2023-11-20 20:46:26 +0100

    Initial commit

---
+++ data-type/src/main/java/com/newproject/datatype/DataTypeApplication.java
@@ -0,0 +1,13 @@
+package com.newproject.datatype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTypeApplication.class, args);
+	}
+
+}
---
+++ data-type/src/test/java/com/newproject/datatype/DataTypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newproject.datatype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
